58b1f9adcc82daed0274a1d0f97cf042695e1d7a
adamseid <dmsd2916@gmail.com>
2026-04-06 16:45:20 -0700

    updated frontend

--- src/api/client.ts
+++ src/api/client.ts
@@ -11,7 +11,7 @@ type ApiErrorResponse = {
 let csrfToken: string | null = null;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   withCredentials: true,
 });
 
